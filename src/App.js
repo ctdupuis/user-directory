bb81331e6825e3eae4d2f87bd19d6e8a75dc26f1
Cody <relationalprogrammer@gmail.com>
2021-12-16 09:38:02 -0600

    Header with styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         
       </header>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Header/Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h2>Home</h2>
+        </header>
+    )
+}
