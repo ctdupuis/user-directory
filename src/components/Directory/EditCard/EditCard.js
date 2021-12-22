45a2938d081f1d489dc301f1758b20886891a34a
Cody <relationalprogrammer@gmail.com>
2021-12-22 10:42:40 -0600

    implemented custom hook, need to set it to update now

--- src/components/Directory/EditCard/EditCard.js
+++ src/components/Directory/EditCard/EditCard.js
@@ -4,7 +4,26 @@ import { useInput } from '../../../hooks/UseInput';
 export default function EditCard({ user, min, max }) {
     const favMovies = user.favoriteMovies.map((movie, idx) => <li key={idx}><input type="text" value={movie} /></li>)
 
-    const {value, bind, reset} = useInput(user.name.first)
+    const { value:firstName, bind:bindFirstName, reset:resetFirstName } = useInput(user.name.first);
+    const { value:lastName, bind:bindLastName, reset:resetLastName } = useInput(user.name.last);
+    const { value:city, bind:bindCity, reset:resetCity} = useInput(user.city);
+    const { value:country, bind:bindCountry, reset:resetCountry} = useInput(user.country);
+    const { value:title, bind:bindTitle, reset:resetTitle} = useInput(user.title);
+    const { value:employer, bind:bindEmployer, reset:resetEmployer} = useInput(user.employer)
+
+    const handleClick = () => {
+        let obj = {
+            name: {
+                first: firstName,
+                last: lastName
+            },
+            city: city,
+            country: country,
+            title: title,
+            employer: employer
+        }
+        console.log(obj)
+    }
 
     return (
         <div className="user-card">
@@ -12,18 +31,19 @@ export default function EditCard({ user, min, max }) {
                 <h3>{user.id}/{max}</h3>
             </div>
             <div className="title">
-                <h3 style={{marginRight: "2%"}}>First: </h3> <input style={{marginRight: "2%"}} type="text" value={value} {...bind} />  <h3 style={{marginRight: "2%"}}>Last: </h3> <input type="text" value={user.name.last} />
+                <h3 style={{marginRight: "2%"}}>First: </h3> <input style={{marginRight: "2%"}} type="text" defaultValue={firstName} {...bindFirstName} /> 
+                <h3 style={{marginRight: "2%"}}>Last: </h3> <input type="text" defaultValue={lastName} {...bindLastName} />
             </div>
             <div className="user-info">
                 <div className="location">
-                    <h3 style={{ display: "inline" }}>City: </h3> <input type="text" value={user.city} />
-                    <h3 style={{ display: "inline", marginLeft: "2%" }}>Country: </h3><input type="text" value={user.country} />
+                    <h3 style={{ display: "inline" }}>City: </h3> <input type="text" value={city} {...bindCity}/>
+                    <h3 style={{ display: "inline", marginLeft: "2%" }}>Country: </h3><input type="text" defaultValue={country} {...bindCountry} />
                 </div>
                 <div className="job-title">
-                    <strong>Job Title:</strong> <input type="text" value={user.title} />
+                    <strong>Job Title:</strong> <input type="text" defaultValue={title} {...bindTitle} />
                 </div>
                 <div className="employer">
-                    <strong>Employer:</strong> <input type="text" value={user.employer} />
+                    <strong>Employer:</strong> <input type="text" defaultValue={employer} {...bindEmployer} />
                 </div>
                 <div className="movies">
                     <strong>Favorite Movies:</strong>
@@ -31,7 +51,7 @@ export default function EditCard({ user, min, max }) {
                         {favMovies}
                     </ol>
                 </div>
-                <button className="crud">Save</button>
+                <button className="crud" onClick={handleClick}>Save</button>
             </div>
         </div>
     )
